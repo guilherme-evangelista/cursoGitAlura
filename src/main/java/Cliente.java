91f4840fc1b774a518d1de52bd2071dc98cbb6d9
Guilherme Gomes Evangelista <ggomes61.gg@gmail.com>
2020-07-15 06:04:03 -0300

    adicionando projeto ao versionamento

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+
+    private Integer idade;
+
+    public Cliente(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        new Cliente(30);
+        clientes();
+    }
+
+    private static void clientes() {
+        new Cliente(30);
+        new Cliente(30);
+        new Cliente(30);
+    }
+}
